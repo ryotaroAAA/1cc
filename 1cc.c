4067e69ed5aa430a30ca4c0151833a734470772c
JP\0300004144 <Ryotaro.Suzuki@sony.com>
2019-06-27 15:58:49 +0900

    Add Node structure, mul, expr, term functions

--- 1cc.c
+++ 1cc.c
@@ -4,20 +4,34 @@
 #include<string.h>
 #include<stdarg.h>
 
-// token type enum
 enum {
-	TK_NUM =256, // integer token
-	TK_EOF,		// EOF token
+	TK_NUM =256,
+	TK_EOF,
 };
 
-// token type
 typedef struct {
 	int ty;
 	int val;
 	char *input;
 } Token;
 
+typedef struct Node{
+	int ty;
+	struct Node *lhs;
+	struct Node *rhs;
+	int val;
+} Node;
+
+enum {
+	ND_NUM = 256,
+};
+
+Node *expr();
+Node *mul();
+Node *term();
+
 char *user_input;
+int pos = 0;
 
 Token tokens[100];
 
@@ -30,12 +44,76 @@ void error(char *fmt, ...){
 }
 
 void error_at(char *loc, char *msg){
-	int pos = loc - user_input;
+	int posi = loc - user_input;
 	fprintf(stderr, "%s\n", user_input);
-	fprintf(stderr, "%*s\n", pos, "");
+	fprintf(stderr, "%*s\n", posi, "");
 	fprintf(stderr, "^ %s\n", msg);
 	exit(1);
 }
+
+Node *new_node(int ty, Node *lhs, Node *rhs) {
+	Node *node = malloc(sizeof(Node));
+	node->ty = ty;
+	node->lhs = lhs;
+	node->rhs = rhs;
+	return node;
+}
+
+Node *new_node_num(int val) {
+	Node *node = malloc(sizeof(Node));
+	node->ty = ND_NUM;
+	node->val = val;
+	return node;
+}
+
+int consume(int ty) {
+	if (tokens[pos].ty != ty)
+		return 0;
+	pos++;
+	return 1;
+}
+
+Node *expr(){
+	Node *node = mul();
+
+	for (;;) {
+		if (consume('+'))
+			node = new_node('+', node, mul());
+		else if (consume('-'))
+			node = new_node('-', node, mul());
+		else 
+			return node;
+	}
+}
+
+Node *mul(){
+	Node *node = term();
+
+	for (;;) {
+		if (consume('*'))
+			node = new_node('*', node, term());
+		else if (consume('/'))
+			node = new_node('/', node, term());
+		else 
+			return node;
+	}
+}
+
+Node *term() {
+	if (consume('(')) {
+		Node *node = expr();
+		if (!consume(')'))
+			error_at(tokens[pos].input, "開きカッコに対応する閉じカッコがありません");
+		return node;
+	}
+
+	if (tokens[pos].ty == TK_NUM)
+		return new_node_num(tokens[pos++].val);
+
+	error_at(tokens[pos].input,
+		"数値でも開きカッコでもないトークンです");
+}
+
 
 void tokenize() {
 	char *p = user_input;
@@ -69,6 +147,33 @@ void tokenize() {
 	tokens[i].input = p;
 }
 
+void gen(Node *node){
+	if (node->ty == ND_NUM) {
+		printf("	push %d\n", node->val);
+		return;
+	}
+	gen(node->lhs);
+	gen(node->rhs);
+
+	printf("	pop rdi\n");
+	printf("	pop rax\n");
+
+	switch (node->ty) {
+		case '+':
+			printf("	add rax, rdi\n");
+		case '-':
+			printf("	sub rax, rdi\n");
+		case '*':
+			printf("	imul rdi\n");
+		case '/':
+			printf("	cqo\n");
+			printf("	idiv rdi\n");
+	}
+
+	printf("	push rax\n");
+}
+
+
 int main (int argc, char **argv) {
 	if (argc != 2) {
 		fprintf(stderr, "引数の個数が正しくありません\n");
@@ -77,40 +182,15 @@ int main (int argc, char **argv) {
 
 	user_input = argv[1];
 	tokenize();
+	Node *node = expr();
 	
 	printf(".intel_syntax noprefix\n");
 	printf(".global main\n");
 	printf("main:\n");
 
-	if (tokens[0].ty != TK_NUM) {
-		error_at(tokens[0].input, "is not number");
-	}
-	printf("	mov rax, %d\n", tokens[0].val);
+	gen(node);
 
-	int i=1;
-	while (tokens[i].ty != TK_EOF) {
-		if (tokens[i].ty == '+') {
-			i++;
-			if (tokens[i].ty != TK_NUM) {
-				error_at(tokens[i].input, "is not number");
-			}
-			printf("	add rax, %d\n", tokens[i].val);
-			i++;
-			continue;
-		}
-		if (tokens[i].ty == '-') {
-			i++;
-			if (tokens[i].ty != TK_NUM) {
-				error_at(tokens[i].input, "is not number");
-			}
-			printf("	sub rax, %d\n", tokens[i].val);
-			i++;
-			continue;
-		}
-		error_at(tokens[i].input, "not expected value...");
-		return 1;
-	}
-
+	printf("	pop rax\n");
 	printf("	ret\n");
 	return 0;
 }
